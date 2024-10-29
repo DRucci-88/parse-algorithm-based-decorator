8752c9d0dfee61c4ab23375e30a39238d6563031
DRucci-88 <d.rucci.2001@gmail.com>
2024-10-29 14:58:48 +0700

    Telegram Layer String, Number, List Success

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 "use strict";
 
 import { Logger } from "sitka";
-import { parseStringToObject } from "./Parse";
-import { Dog } from "./Dto";
+// import { parseStringToObject } from "./Parse";
+// import { Dog } from "./Dto";
+import { convertStringToObject } from "./telegram/Decoder";
+import { Car } from "./dto/Car";
 
 export class Example {
   /* Private Instance Fields */
@@ -23,10 +25,18 @@ export class Example {
   }
 }
 
-const input = "AsepBudi9870002akuka90110001leo";
-console.log(input);
+// const input = "AsepBudi9870002akuka90110001leo";
+// console.log(input);
 
-const parsed = parseStringToObject(input, Dog);
+const input =
+  "Avanza    Ride In Comfort     02420000000000000004" +
+  "Potenza   Performance Boost   0323600000Murah Meriah Banget " +
+  "Turanza   Fuel Efficiency     0323600000Durability Patent   " +
+  "Alenza    CUVs & SUVs         0323600000Favorite BapakBapack" +
+  "Duravis   Durable & Endurance 0323600000Bisa menyelam laut  ";
+
+const parsed = convertStringToObject(input, Car);
+// const parsed = parseStringToObject(input, Dog);
 
 console.log("RESULT");
 console.log(parsed);
